72087a7e1d379010e30a23eba29aaa9cb376a359
phcavalcante7 <pecavalcante08@gmail.com>
2025-05-30 16:48:57 -0300

    updating branchs

---
+++ src/app/components/Camiseta.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Camiseta() {
+  return (
+    <div>
+      <Image src="/camisetas/grafite/grafite_2_camisetas_preta.jpg" alt="Grafite" width={500} height={500} />
+    </div>
+  );
+}
